c03a4478f30e1b55407b3923908b5ef136d76abe
erkaltufekci <erkaltufekci@gmail.com>
2021-07-23 10:40:37 +0200

    Add negative statement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function centuryFind(year) {
   if (isNaN(year) || !Number.isInteger(year)) throw new Error('Year should be an integer!')
+  if (year < 0) return `B.C. ${-Math.floor(year / 100)}`
   return Math.ceil(year / 100)
 }
 module.exports = centuryFind
