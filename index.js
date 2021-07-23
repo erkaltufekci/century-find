e68ddc2493226389bef9f2af75c417a2b6ef193a
erkaltufekci <erkaltufekci@gmail.com>
2021-07-23 15:58:13 +0200

    Fixed index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function centuryFind(year) {
-  if (isNaN(year) || !Number.isInteger(year)) throw new Error('Year should be an integer!')
+  if (Number.isNaN(year) || !Number.isInteger(year)) throw new Error('Year should be an integer!')
   if (year < 0) return `B.C. ${-Math.floor(year / 100)}`
   return Math.ceil(year / 100)
 }
