230cf82e526bd69505828a8c369485abbbaf765f
erkaltufekci <erkaltufekci@gmail.com>
2021-07-23 10:44:12 +0200

    Update test cases

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,14 +8,62 @@ describe('century find test', () => {
   })
 
   it('convert the year to century', () => {
-    const result = centuryFind(1000)
+    const result = centuryFind(100)
 
-    expect(result).toBe(10)
+    expect(result).toBe(1)
+  })
+
+  it('convert the year to century', () => {
+    const result = centuryFind(499)
+
+    expect(result).toBe(5)
+  })
+
+  it('convert the year to century', () => {
+    const result = centuryFind(750)
+
+    expect(result).toBe(8)
+  })
+
+  it('convert the year to century', () => {
+    const result = centuryFind(1923)
+
+    expect(result).toBe(20)
+  })
+
+  it('convert the year to century', () => {
+    const result = centuryFind(4684)
+
+    expect(result).toBe(47)
+  })
+
+  it('convert the year to century', () => {
+    const result = centuryFind(-105)
+
+    expect(result).toBe('B.C. 2')
+  })
+
+  it('convert the year to century', () => {
+    const result = centuryFind(-395)
+
+    expect(result).toBe('B.C. 4')
   })
 
   test('convert the year to century', () => {
     expect(() => {
       centuryFind('abc')
+    }).toThrow()
+  })
+
+  test('convert the year to century', () => {
+    expect(() => {
+      centuryFind(hjöäh)
+    }).toThrow()
+  })
+
+  test('convert the year to century', () => {
+    expect(() => {
+      centuryFind('123')
     }).toThrow()
   })
 })
